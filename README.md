b84f3d28278581c9724a9b8e869af8bb8e6b90e3
SharksZhang <2425194544@qq.com>
2018-10-26 00:25:27 +0800

    add web crawler

--- README.md
+++ README.md
@@ -1,6 +1,51 @@
-# godemo
-##gotour
-exercise for golang.org go tour
+## godemo
+### gotour
+golang.org中gotour部分的练习。
+#### exercise_web_crawler
+1.如何确保主线程先不退出？
+创建一个管道，用于父协程与子协程之间的通信。在子协程完成后通知父协程。
+2.如何在多线程时保证不重复？
+创建一个全局的map，用于保存所有url是否访问过的状态。
+3.定义只使用一次的struct,单例模式的高级实现
 
-##mytestdemo
+```
+var fetched = struct {
+	m map[string]error
+	sync.Mutex
+}{m: make(map[string]error)}
+
+```
+4.错误记录
+
+如下代码，未将for range出的u 值做为参数传给子协程， 导致子协程取到的url都是相同地。
+```
+for i, u := range urls {
+			fmt.Printf("-> Crawling child %v/%v of %v : %v.\n", i, len(urls), url, u)
+
+			go func() {
+				Crawl(u, depth-1, fetcher)
+				done <- "true"
+			}()
+		}
+
+```
+
+如下形式也不行，因为在子协程运行时，取到的i值也是相同的
+    
+```
+for i, u := range urls {
+			fmt.Printf("-> Crawling child %v/%v of %v : %v.\n", i, len(urls), url, u)
+
+			go func() {
+				Crawl(urls[i], depth-1, fetcher)
+				done <- "true"
+			}()
+		}
+
+```
+
+
+
+
+## mytestdemo
 experiment for go
--- gotour/exercise_web_crawler.go/exercise_web_crawler.go
+++ gotour/exercise_web_crawler.go/exercise_web_crawler.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"errors"
 	"fmt"
+	"sync"
 )
 
 type Fetcher interface {
@@ -9,28 +11,94 @@ type Fetcher interface {
 	Fetch(url string) (body string, urls []string, err error)
 }
 
+var fetchedMap = struct {
+	m    map[string]error
+	lock sync.Mutex
+}{m: make(map[string]error, 0)}
+var loading = errors.New("loading")
+
 // Crawl 使用 fetcher 从某个 URL 开始递归的爬取页面，直到达到最大深度。
 func Crawl(url string, depth int, fetcher Fetcher) {
 	// TODO: 并行的抓取 URL。
 	// TODO: 不重复抓取页面。
 	// 下面并没有实现上面两种情况：
 	if depth <= 0 {
+		fmt.Printf("<- Done with %v, depth 0.\n", url)
 		return
 	}
+
+	fetchedMap.lock.Lock()
+	if _, ok := fetchedMap.m[url]; ok {
+		fmt.Printf("<- Done with %v, already fetched.\n", url)
+		fetchedMap.lock.Unlock()
+		return
+	}
+	fetchedMap.m[url] = loading
+	fetchedMap.lock.Unlock()
+
 	body, urls, err := fetcher.Fetch(url)
+
+	fetchedMap.lock.Lock()
+	fetchedMap.m[url] = err
+	fetchedMap.lock.Unlock()
+
 	if err != nil {
-		fmt.Println(err)
+		fmt.Printf("<- Error on %v: %v\n", url, err)
 		return
 	}
-	fmt.Printf("found: %s %q\n", url, body)
-	for _, u := range urls {
-		Crawl(u, depth-1, fetcher)
+
+	fmt.Printf("Found: %s %q\n", url, body)
+	done := make(chan string)
+
+	/*如下代码，未将for range出的u 值做为参数传给子协程， 导致子协程取到的url都是相同地。
+		for i, u := range urls {
+			fmt.Printf("-> Crawling child %v/%v of %v : %v.\n", i, len(urls), url, u)
+
+			go func() {
+				Crawl(u, depth-1, fetcher)
+				done <- "true"
+			}()
+		}
+
+	如下形式也不行，因为在子协程运行时，i值已经变为最终值
+		for i, u := range urls {
+			fmt.Printf("-> Crawling child %v/%v of %v : %v.\n", i, len(urls), url, u)
+
+			go func() {
+				Crawl(urls[i], depth-1, fetcher)
+				done <- "true"
+			}()
+		}
+
+	*/
+	for i, u := range urls {
+		fmt.Printf("-> Crawling child %v/%v of %v : %v.\n", i, len(urls), url, u)
+
+		go func(url string) {
+			Crawl(url, depth-1, fetcher)
+			done <- "true"
+		}(u)
 	}
-	return
+
+	for i, u := range urls {
+		fmt.Printf("<- [%v] %v/%v Waiting for child %v.\n", url, i, len(urls), u)
+		<-done
+	}
+	fmt.Printf("<- Done with %v\n", url)
+
+	//return
 }
 
 func main() {
 	Crawl("https://golang.org/", 4, fetcher)
+	fmt.Println("Fetching stats\n--------------")
+	for url, err := range fetchedMap.m {
+		if err != nil {
+			fmt.Printf("%v failed: %v\n", url, err)
+		} else {
+			fmt.Printf("%v was fetched\n", url)
+		}
+	}
 }
 
 // fakeFetcher 是返回若干结果的 Fetcher。
@@ -55,6 +123,7 @@ var fetcher = fakeFetcher{
 		[]string{
 			"https://golang.org/pkg/",
 			"https://golang.org/cmd/",
+			"https://golang.org/cmd1/",
 		},
 	},
 	"https://golang.org/pkg/": &fakeResult{
